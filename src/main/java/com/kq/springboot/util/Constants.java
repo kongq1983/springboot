d6702ddc41898831d6bc60b0aec26369785d74d2
king <king@qq.com>
2019-08-29 19:18:12 +0800

    add constants

---
+++ src/main/java/com/kq/springboot/util/Constants.java
@@ -0,0 +1,16 @@
+package com.kq.springboot.util;
+
+/**
+ * Constants
+ *
+ * @author kq
+ * @date 2019-08-29
+ */
+public class Constants {
+
+    public static final int OPEN = 1;
+    private static final int WINDOWS = 1;
+
+    public static final String NAME = "king";
+
+}
